afd29edb4ef803942634d6b8ab6605657f266132
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-30 05:04:47 +0100

    .

---
+++ closure.js
@@ -0,0 +1,8 @@
+function outer(){
+    const outerVar = 'Hey I am the outer Var';
+    function inner(){
+        const innerVar = "hey I am an inner var";
+        console.log(innerVar);
+        console.log(outerVar);
+    }
+}
