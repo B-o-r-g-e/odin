922faa9c7844ccf21a865c71cc4e6a531b6d51c3
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-01-30 05:47:35 +0100

    .

--- closure.js
+++ closure.js
@@ -9,11 +9,23 @@
 // }
 // outer()
 
-function outer(){
-    const outerVar = 'Hey I am the outer Var';
-    return function inner(){
-        const innerVar = "hey I am an inner var";
-        console.log(innerVar);
-        console.log(outerVar);
-    }
+// function outer(){
+//     const outerVar = 'Hey I am the outer Var';
+//     function inner(){
+//         const innerVar = "hey I am an inner var";
+//         console.log(innerVar);
+//         console.log(outerVar);
+//     }
+//     return inner
+// }
+//
+// const innerFn = outer();
+//
+// innerFn()
+
+function createGreeting(greeting = "") {
+    const myGreet = greeting.toUpperCase();
+    return function(name) {
+        return `${myGreet} ${name}`;
+    };
 }
